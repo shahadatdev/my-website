9cc126037bd512ccf1d58892f882473821ac144b
shahadatdev <shahadat_lmu@hotmail.com>
2020-03-03 16:25:51 -0600

    commit

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
-$(document).ready(function(){
-
+$(document).ready(function()
+{
         $('#itemslider').carousel({ interval: 3000 });
         
-        $('.carousel-showmanymoveone .item').each(function(){
-        var itemToClone = $(this);
+        $('.carousel-showmanymoveone .item').each(function()
+        {
+                var itemToClone = $(this);
         
-        for (var i=1;i<6;i++) {
-        itemToClone = itemToClone.next();
+                for (var i=1;i<6;i++) 
+                {
+                        itemToClone = itemToClone.next();
         
-        if (!itemToClone.length) {
-        itemToClone = $(this).siblings(':first');
-        }
+                        if (!itemToClone.length) 
+                        {
+                                itemToClone = $(this).siblings(':first');
+                        }
         
-        itemToClone.children(':first-child').clone()
-        .addClass("cloneditem-"+(i))
-        .appendTo($(this));
-        }
+                        itemToClone.children(':first-child').clone()
+                        .addClass("cloneditem-"+(i))
+                        .appendTo($(this));
+                }
         });
-        });
+});
         
